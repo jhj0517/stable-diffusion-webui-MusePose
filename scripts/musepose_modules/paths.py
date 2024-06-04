a89fd9d11a538b1afff67284f3fa32bf48dc234b
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-06-05 00:20:16 +0900

    update to make dir

--- scripts/musepose_modules/paths.py
+++ scripts/musepose_modules/paths.py
@@ -1,6 +1,10 @@
 import os
 
-extension_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+extension_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sd_webui_dir = os.path.abspath(os.path.join(extension_dir, '..', '..'))
 pose_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "pose"))
+if not os.path.exists(pose_output_dir):
+    os.makedirs(pose_output_dir)
 final_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "final"))
+if not os.path.exists(final_output_dir):
+    os.makedirs(final_output_dir)
