00b7725d25e1d2c7b0ab91df26d6fd8a55e1dbdc
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-06-05 20:53:19 +0900

    rename folder path

--- scripts/musepose_modules/paths.py
+++ scripts/musepose_modules/paths.py
@@ -6,11 +6,11 @@ musepose_module_dir = os.path.abspath(os.path.join(extension_dir, 'scripts', 'mu
 
 sd_webui_dir = os.path.abspath(os.path.join(extension_dir, '..', '..'))
 
-pose_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "pose"))
+pose_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "pose_alignment"))
 if not os.path.exists(pose_output_dir):
     os.makedirs(pose_output_dir)
 
-final_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "final"))
+final_output_dir = os.path.abspath(os.path.join(sd_webui_dir, 'outputs', 'MusePose', "musepose_inference"))
 if not os.path.exists(final_output_dir):
     os.makedirs(final_output_dir)
 
