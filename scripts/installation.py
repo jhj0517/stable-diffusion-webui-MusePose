2985292d1651cb8510f033e1f37a09339d6829db
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-06-08 00:12:50 +0900

    fix bug and remove xformers

--- scripts/installation.py
+++ scripts/installation.py
@@ -5,11 +5,11 @@ import re
 from scripts.musepose_modules.paths import *
 
 def install_package(install_command):
-    print(f"MusePose Extension: Installing {install_command}")
     if "sys_platform" in install_command:
         command = [f"{install_command}"]
     else:
         command = install_command.split()
+        print(f"MusePose Extension: Installing {install_command}")
 
     subprocess.check_call(
         [sys.executable, "-m", "pip", "install"] + command,
@@ -46,18 +46,15 @@ def install_musepose():
             if not is_installed(package_name):
                 install_package(package)
 
-    import torch
     problematic_packages = [
         "wheel==0.43.0",
         "mmcv==2.1.0 --find-links https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html",
         "mmengine==0.10.4",
         "mmdet==3.3.0",
         "mmpose==1.3.1",
-        f"torch=={torch.__version__}",
-        "xformers"
     ]
 
     for command in problematic_packages:
-        package_name = get_package_name(package)
+        package_name = get_package_name(command)
         if not is_installed(package_name):
             install_package(command)
