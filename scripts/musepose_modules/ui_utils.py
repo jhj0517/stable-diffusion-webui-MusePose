de334171de18e2762ff9d9fb31185d63605c234e
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-06-13 18:33:34 +0900

    add automatic input to step2

--- scripts/musepose_modules/ui_utils.py
+++ scripts/musepose_modules/ui_utils.py
@@ -1,8 +1,16 @@
 import os
-
+import gradio as gr
 
 def open_folder(folder_path):
     if os.path.exists(folder_path):
         os.system(f'start "" "{folder_path}"')
     else:
         print(f"The folder '{folder_path}' does not exist.")
+
+def on_step1_complete(input_img: str, input_pose_vid: str):
+    print(input_img)
+    print(input_pose_vid)
+    temp_folder = os.path.dirname(input_img)
+
+    return [gr.Image(label="Input Image", value=input_img, type="filepath", scale=5),
+            gr.Video(label="Input Aligned Pose Video", value=input_pose_vid, scale=5)]
